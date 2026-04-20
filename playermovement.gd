d322684191169875d31248de7207f3bec8c644d0
Santiago Gauto <73668218+Santiago6666@users.noreply.github.com>
2026-04-20 16:20:04 -0300

    se agrega correr
    se agrega correr presionando la misma dirección 2 veces

--- playermovement.gd
+++ playermovement.gd
@@ -13,11 +13,9 @@ var d_jump = false
 @onready var sprite2d=$Sprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
@@ -28,10 +26,11 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction != 0:
+		velocity.x = direction * (RUN_SPEED if is_running else SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
+		is_running = false
 	animations(direction)
 	
 	move_and_slide()
@@ -40,6 +39,18 @@ func _physics_process(delta: float) -> void:
 		sprite2d.flip_h =false
 	elif direction ==-1:
 		sprite2d.flip_h =true
+		
+func _input(event):
+	if event.is_action_pressed("ui_right"):
+		var current_time = Time.get_ticks_msec() / 1000.0
+		if current_time - last_tap_time < double_tap_time:
+			is_running = true
+		last_tap_time = current_time
+	if event.is_action_pressed("ui_left"):
+		var current_time = Time.get_ticks_msec() / 1000.0
+		if current_time - last_tap_time < double_tap_time:
+			is_running = true
+		last_tap_time = current_time
 func animations(direction):
 	if is_on_floor():
 		if direction==0:
