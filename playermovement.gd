c2f60e35cfee0d9be7b9f10d91f51dabe300b5aa
Santiago Gauto <73668218+Santiago6666@users.noreply.github.com>
2026-04-20 17:09:33 -0300

    se agregan todas las mecanicas de moviemiento
    recreadas: caminar, correr y deslizar (comparten animación) volar, saltar y rebotar  (comparten animación) ,

--- playermovement.gd
+++ playermovement.gd
@@ -8,25 +8,53 @@ var is_running = false
 var last_tap_time = 0.0
 var double_tap_time = 0.3
 var d_jump = false
+var is_sliding = false
+var slide_friction = 600
+var was_on_floor = false
+var can_bounce = false
+var jump_count = 0
+var max_jumps = 2
+var is_flying = false
+var float_gravity = 100
 
 @onready var animation=$AnimationPlayer
 @onready var sprite2d=$Sprite2D
 
 func _physics_process(delta: float) -> void:
+	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		if is_flying:
+			velocity.y += float_gravity * delta
+			
+		else:
+			velocity += get_gravity() * delta
+	if is_flying:
+		velocity.y = min(velocity.y, 80)
+	if is_flying and not Input.is_action_pressed("ui_accept"):
+		is_flying = false
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
-			d_jump = true
-		elif d_jump:
-			velocity.y = JUMP_VELOCITY * 0.8 
-			d_jump = false
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+			jump_count = 1
+	
+		elif jump_count < max_jumps:
+			velocity.y = JUMP_VELOCITY * 0.8
+			jump_count += 1
+	
+		else:
+			is_flying = true
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
+
+	if is_on_floor() and Input.is_action_pressed("ui_down") and direction != 0 and not is_sliding:
+		is_sliding = true
+		velocity.x = direction * RUN_SPEED 
+
+	if is_sliding:
+		velocity.x = move_toward(velocity.x, 0, slide_friction * delta)
+		if abs(velocity.x) < 10:
+			velocity.x = 0
+			is_sliding = false
+	elif direction != 0:
 		velocity.x = direction * (RUN_SPEED if is_running else SPEED)
 	else:
 		velocity.x = 0
@@ -34,12 +62,19 @@ func _physics_process(delta: float) -> void:
 	animations(direction)
 	
 	move_and_slide()
-	
+	var just_landed = not was_on_floor and is_on_floor()
+	if just_landed and can_bounce:
+		velocity.y = -120
+		can_bounce = false
+		was_on_floor = is_on_floor()
+	if is_on_floor():
+		jump_count = 0
+		is_flying = false
 	if direction ==1:
 		sprite2d.flip_h =false
 	elif direction ==-1:
 		sprite2d.flip_h =true
-		
+	was_on_floor = is_on_floor()
 func _input(event):
 	if event.is_action_pressed("ui_right"):
 		var current_time = Time.get_ticks_msec() / 1000.0
@@ -52,18 +87,15 @@ func _input(event):
 			is_running = true
 		last_tap_time = current_time
 func animations(direction):
-	if is_on_floor():
-		if direction==0:
+	if is_sliding:
+		animation.play("slide")
+	elif is_on_floor():
+		if direction == 0:
 			animation.play("idle")
 		else:
 			animation.play("run")
 	else:
-		if velocity.y<0:
+		if velocity.y < 0:
 			animation.play("jump")
-		elif velocity.y>0:
+		elif velocity.y > 0:
 			animation.play("fall")
-func double_jump(d_jump):
-	if is_on_floor():
-		d_jump=false
-	else:
-		d_jump=true
